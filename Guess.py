0530ce426e441867eca4d13e773139bac5f6afee
K-KingH <47615200+K-KingH@users.noreply.github.com>
2020-03-23 14:32:52 -0400

    Number Guessing Game(updated)
    Functional Version of previous upload. Will create a GUI game-like version later on.

--- Guess.py
+++ Guess.py
@@ -1,15 +1,21 @@
 import random
-user = input('Guess a number between 0 and 20. To end game enter "done"')
-rand = random.uniform(0, 20)
-while True:
-  if user == rand:
-    print("Correct!")
-    cnt += 1
-    if cnt >=4:
-      print("You're amazing!")
-  elif user == 'done':
-    break
-  else:
-    print("Incorrect!")
+user = input('To end game enter "-10". Press Enter key to begin.')
+while user != "Exit":
+    rand = random.randint(0, 20)
+    user = int(input("Guess a number between 0 and 20."))
     cnt = 0
+    streak = 0
+    if user == rand:
+        print("Correct!")
+        cnt += 1
+        streak += 1
+        if streak == 4:
+            print("You're amazing!")
+    elif user == -10:
+        break
+    else:
+        print("Incorrect! " + str(rand) + " was the correct answer.")
+        streak = 0
+print("Finish!")
+print("You guessed correctly " + str(cnt) + " times!")
     
